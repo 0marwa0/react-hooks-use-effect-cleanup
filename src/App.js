c43e1c43dca5b8af88ec732f9268c62c27859cad
marwa <marwajawad19@gmail.com>
2021-08-05 04:17:33 -0400

    Adding a ⏰

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React, { useState } from "react";
-
+import React, { useEffect, useState } from "react";
+import Clock  from "./Clock";
 function App() {
   const [showClock, setShowClock] = useState(true);
+  const [time ,setTime]=useState(new Date())
+useEffect(()=>{
+const timer=setInterval(() => {
+  setTime(new Date())
+}, 1000);
+return ()=>{
+  clearInterval(timer)
+}
 
+},[])
   return (
     <div>
-      {showClock ? <Clock /> : null}
-      <button onClick={() => setShowClock(false)}>Toggle Clock</button>
+{time.toString()}
     </div>
   );
 }
